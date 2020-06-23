6dc840344f782e1703dfc0767edeefce435e29e4
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-06-23 12:21:35 -0500

    Clase 15

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,18 @@
 module.exports = {
 	plugins: [
-     		require('postcss-preset-env')({
-      			autoprefixer: {
-							grid:true,
-							flexbox:false,
-						 },
-						 customProperties: false
-     		})
+		require('postcss-import')({
+		}),
+		require('postcss-apply'),
+		require('postcss-custom-selector'),
+		require('postcss-custom-media')({
+			preserve:false
+		}),
+		require('postcss-preset-env')({
+				autoprefixer: {
+					grid:true,
+					flexbox:false,
+					},
+					customProperties: false
+		})
   ]
 }
